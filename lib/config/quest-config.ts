6810830653c1a37d43155dd48a45c5017927312d
Felipe Sousa <felipepedrosousa@hotmail.com>
2026-03-17 21:51:29 -0300

    Update WhatsApp URL mappings in quest-config.ts to new sendflow links for all temperature keys.

--- lib/config/quest-config.ts
+++ lib/config/quest-config.ts
@@ -2,10 +2,10 @@ export const DEFAULT_QUEST_FORM_VERSION_ID =
   "a9e5538c-ee07-41e4-95a0-862e89adf186";
 
 const QUEST_WHATSAPP_URL_BY_TEMPERATURE = {
-  f: "https://redirects.aliancadivergente.com.br/oro-pages-f",
-  org: "https://redirects.aliancadivergente.com.br/oro-pages-org",
-  m: "https://redirects.aliancadivergente.com.br/oro-pages-m",
-  q: "https://redirects.aliancadivergente.com.br/oro-pages-f",
+  f: "https://sendflow.click/i/oramar26",
+  org: "https://sendflow.click/i/oramar26",
+  m: "https://sendflow.click/i/oramar26",
+  q: "https://sendflow.click/i/oramar26",
 } as const;
 
 type QuestTesteTemperatureKey = keyof typeof QUEST_WHATSAPP_URL_BY_TEMPERATURE;
