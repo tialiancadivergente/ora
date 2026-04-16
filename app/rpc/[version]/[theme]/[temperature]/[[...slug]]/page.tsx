2071620f0c089b6a632daaab43b4a44ec5a256d2
Felipe Sousa <felipepedrosousa@hotmail.com>
2026-04-16 13:41:29 -0300

    Add new quiz components and layout: implement ContainerQuest and RpcV1 components for quiz functionality, create layout for quiz pages, and update text in the quiz container for clarity. Introduce new page structure for handling quiz interactions and lead capture.

---
+++ app/rpc/[version]/[theme]/[temperature]/[[...slug]]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SplashScreenOro from "@/app/components/SplashScreen/SplashScreenOro";
+import RpcV1 from "../../../v1";
+
+export default function Home() {
+
+  return (
+    <SplashScreenOro>
+      <RpcV1 />
+    </SplashScreenOro>
+  );
+}
